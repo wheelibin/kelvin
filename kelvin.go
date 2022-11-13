a7d2e6589bf6e0eb02af1dce4e251e18a7c18f3a
wheelibin <wheelibin@gmail.com>
2022-11-13 13:01:05 +0000

    feat: Add -list command line flag to list devices without controlling any lights

--- kelvin.go
+++ kelvin.go
@@ -45,6 +45,7 @@ var flagEnableUpdates = flag.Bool("enableUpdates", true, "Enable automatic updat
 var flagEnableWebInterface = flag.Bool("enableWebInterface", false, "Enable the web interface at startup")
 var flagDisableRateLimiting = flag.Bool("disableRateLimiting", false, "Disable the limiting of requests to the hue bridge")
 var flagDisableHTTPS = flag.Bool("disableHTTPS", false, "Disable HTTPS for the connection to the hue bridge")
+var flagListDevices = flag.Bool("list", false, "List all devices and exit")
 
 var configuration *Configuration
 var bridge = &HueBridge{}
@@ -112,6 +113,12 @@ func main() {
 		log.Warning(err)
 	}
 	printDevices(l)
+
+	// if list devices flag is set then simply exit after listing them
+	if *flagListDevices {
+		os.Exit(0)
+	}
+
 	for _, light := range l {
 		light := light
 
